bfccc2aa73f50e16420232dcc4a40171000e674b
aguozden <agustina.guozden@gmail.com>
2026-01-08 16:17:10 -0300

    Clean Vite boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+//sugerencia de chat gpt:
+    //import React from 'react';
+    //import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.tsx'
 
