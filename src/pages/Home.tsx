7e8256f69fbfe96d5f59662dbd92bb5294bab411
aguozden <agustina.guozden@gmail.com>
2026-01-12 15:06:49 -0300

    Add Login page and navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <section>
       <h1>Marketplace solidario</h1>
       <p>Conectando necesidades con capacidades</p>
+
+      <Link to="/login">Ir a Login</Link>
     </section>
   );
 }
