c183fc7fa430c7de2fc015314c63e6daf5f974a0
aguozden <agustina.guozden@gmail.com>
2026-01-12 15:21:18 -0300

    Add main layout with header navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MainLayout from "./layouts/Mainlayout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
     </Routes>
   );
 }
